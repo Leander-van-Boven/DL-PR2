c45445522a37cd12fc2ccfd4132aca6023b6fa59
Saltpile123 <ikhoudvanberichten@gmail.com>
2021-04-07 17:48:05 +0200

    added dcgan1.py and dcgan2.py removed all shit from gan.py

---
+++ dcgan1.py
@@ -0,0 +1,82 @@
+#####################################################################
+# THIS FILE HAS BEEN COPIED FROM THE eriklindernoren/Keras-GAN REPO #
+# then some modifications were made                                 #
+#####################################################################
+
+from __future__ import print_function, division
+
+from tensorflow.keras.layers import Activation, BatchNormalization, Conv2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Input, LeakyReLU
+from tensorflow.keras.layers import Reshape, UpSampling2D, ZeroPadding2D
+from tensorflow.keras.models import Model, Sequential
+
+
+def build_generator1(latent_dim, img_size):
+    (img_row, img_col, channels) = img_size
+
+    row = img_row // 4
+    col = img_col // 4
+
+    assert row*4 == img_row and col*4 == img_col
+
+    model = Sequential()
+
+    model.add(Dense(128 * row * col, activation="relu",
+                    input_dim=latent_dim))
+    model.add(Reshape((row, col, 128)))
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, kernel_size=3, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+    model.add(UpSampling2D())
+    model.add(Conv2D(64, kernel_size=3, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+    model.add(Conv2D(channels, kernel_size=3, padding="same"))
+    model.add(Activation("tanh"))
+
+    model.summary()
+
+    noise = Input(shape=(latent_dim,))
+    img = model(noise)
+
+    return Model(noise, img)
+
+
+def build_discriminator1(img_shape, opt, loss='binary_crossentropy'):
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=3, strides=2,
+                     input_shape=img_shape, padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+    model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.summary()
+
+    img = Input(shape=img_shape)
+    validity = model(img)
+
+    disc = Model(img, validity)
+
+    disc.compile(
+        optimizer=opt,
+        loss=loss,
+        metrics=['accuracy']
+    )
+
+    return disc
