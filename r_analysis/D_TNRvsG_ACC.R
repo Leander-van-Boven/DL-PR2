3921ca4e475ef86c318bc78a07c36a58b052f928
JesperK123456 <47450353+JesperK123456@users.noreply.github.com>
2021-04-12 20:34:18 +0200

    modifier R analysis

--- r_analysis/D_TNRvsG_ACC.R
+++ r_analysis/D_TNRvsG_ACC.R
@@ -6,6 +6,6 @@ library('dplyr')
 
 ggplot(df, aes(x=epoch)) + 
   #geom_point(aes(y=d_acc_fake), color='red', size=.5, alpha=.5) + 
-  geom_smooth(aes(y=d_acc_fake), se=FALSE, color='red') +
+  geom_smooth(aes(y=d_acc_fake), color='red') +
   #geom_point(aes(y=g_acc), color='blue', size=.5, alpha=.5) +
-  geom_smooth(aes(y=g_acc), se=FALSE, color='blue')
+  geom_smooth(aes(y=g_acc), color='blue')
